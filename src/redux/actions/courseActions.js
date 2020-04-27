60f6029a00b56e6d0ace2853aa97b5311cd46eac
rasha-essam <engrasha88@gmail.com>
2020-04-27 16:55:41 +0200

    Adding Action Types as constants and use them in action creators and in reducers

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,7 +1,8 @@
 // Here we'll add our action creators (action creator functions)
+import * as types from './actionTypes';
 
 // object shorthand syntax , we omitted course: course and used it once because the
 // left handside is the same name as the right handside
 export function createCourse(course) {
-  return { type: 'CREATE_COURSE', course };
+  return { type: types.CREATE_COURSE, course };
 }
